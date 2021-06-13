d46288d247f06c7675aa6e8eb1bf9696486dba78
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-13 11:38:05 +0530

    Added use case 2 to check if both lines are equal

--- lineComparison.java
+++ lineComparison.java
@@ -1,30 +1,46 @@
 package com.bridgelabz;
 import java.util.Scanner;
 
-public class lineComparisonUC1 {
-	public static  double lineComparison(int num1, int num2, int num3, int num4) {
+public class lineComparisonUC2 {
+	public static double lineComparison(int num1, int num2, int num3, int num4) {
 		double ans = Math.sqrt(Math.pow( (num3-num1),2 ) + Math.pow( (num4-num2),2));
 		return ans;
 	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to the line comparison program");
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter x1 : ");
-		int num1 = sc.nextInt();
+        // Reading user input
+        Scanner scan = new Scanner(System.in);
 
-		System.out.print("Enter y1 : ");
-		int num2 = sc.nextInt();
+        System.out.print("Enter X1, for line 1 : ");
+        int num1 = scan.nextInt();
+        System.out.print("Enter Y1, for line 1 : ");
+        int num2 = scan.nextInt();
+        System.out.print("Enter X2, for line 1 : ");
+        int num3 = scan.nextInt();
+        System.out.print("Enter Y2, for line 1 : ");
+        int num4 = scan.nextInt();
+        System.out.print("Enter X1, for line 2 : ");
+        int num5 = scan.nextInt();
+        System.out.print("Enter Y1, for line 2 : ");
+        int num6 = scan.nextInt();
+        System.out.print("Enter X2, for line 2 : ");
+        int num7 = scan.nextInt();
+        System.out.print("Enter Y2, for line 2 : ");
+        int num8 = scan.nextInt();
 
-		System.out.print("Enter X2 : ");
-		int num3 = sc.nextInt();
+        scan.close();
 
-		System.out.print("Enter y2 : ");
-		int num4 = sc.nextInt();
+        double result = lineComparison(num1, num2, num3, num4);
+        double result2 = lineComparison(num5, num6, num7, num8);
+        String s1 = String.valueOf(result);
+        String s2 = String.valueOf(result2);
 
-		sc.close();
+        // Displaying distance
+        System.out.println("Length of first line is : "+result);
+        System.out.println("Length of second line is : "+result2);
 
-		double result = lineComparison(num1, num2, num3, num4);
-		System.out.println("The length of the line is " +result);
+        // Equals method
+        System.out.println(s1.equals(s2));
+
 	}
 }
