d69084afafcf0092144cc5aad0b7e9a38c3113f8
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-13 11:31:07 +0530

    Added welcome statement

---
+++ lineComparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class lineComparisonUC1 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the line comparison program");
+	}
+}
